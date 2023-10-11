1d618ee351a0d0e7048b0d4da7752cf8e63ad879
RayaRabbani <rayarabbani522@gmail.com>
2023-10-11 16:13:57 +0700

    event detail

--- resources/views/layouts/front/event/detail-event.blade.php
+++ resources/views/layouts/front/event/detail-event.blade.php
@@ -1,5 +1,50 @@
 @extends('layouts.app-front')
 
 @section('content')
-    <p>es</p>
+
+<div class="container">
+    <div class="gambar-container">
+        <img src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/webinar-banner-template-design-3b874d0c3a80de3fba9ad761596d7911_screen.jpg?ts=1631175223" alt="Iamge Event">
+    </div>
+    <div class="deskripsi-container">
+        <div class="deskripsi-box">
+            <h3>More information</h3><br>
+            <p>𝐇𝐚𝐥𝐨 𝐁𝐚𝐩𝐚𝐤/𝐈𝐛𝐮, 𝐫𝐞𝐤𝐚𝐧 – 𝐫𝐞𝐤𝐚𝐧 𝐔𝐍𝐒𝐈𝐀👋</p><br>
+            <p>𝐓𝐨𝐩𝐢𝐤:
+                Workshop Kurikulum Merdeka Bagi Wakil Kurikulum, Webinar Guru Informatika dan Guru Bimbingan Konseling (BK) SMA/SMK Negeri dan Swasta Se-Jakarta Selatan Wilayah 2.</p><br>
+            <p>𝐏𝐞𝐫𝐥𝐮 𝐝𝐢𝐜𝐚𝐭𝐚𝐭📝: <br>
+                𝐖𝐨𝐫𝐤𝐬𝐡𝐨𝐩 𝐊𝐮𝐫𝐢𝐤𝐮𝐥𝐮𝐦 𝐌𝐞𝐫𝐝𝐞𝐤𝐚 𝐁𝐞𝐥𝐚𝐣𝐚𝐫 𝐉𝐞𝐧𝐣𝐚𝐧𝐠 𝐒𝐌𝐀/𝐒𝐌𝐊.
+                Hari & Tanggal: Kamis, 01 September 2022.
+                Pukul: 08.30 s.d 12.00 WIB.
+                Lokasi: Kampus Menara UNAS, Jl. RM. Harsono, Ragunan, Jakarta Selatan</p><br>
+            <p>𝐖𝐞𝐛𝐢𝐧𝐚𝐫 𝐈𝐧𝐟𝐨𝐫𝐦𝐚𝐭𝐢𝐤𝐚 𝐝𝐚𝐧 𝐖𝐞𝐛𝐢𝐧𝐚𝐫 𝐏𝐞𝐫𝐚𝐧 𝐆𝐮𝐫𝐮 𝐁𝐊 𝐃𝐚𝐥𝐚𝐦 𝐌𝐞𝐦𝐛𝐢𝐧𝐚 𝐊𝐚𝐫𝐢𝐞𝐫 𝐏𝐞𝐬𝐞𝐫𝐭𝐚 𝐃𝐢𝐝𝐢𝐤.</p><br>
+
+            <h3>Event Description </h3><br>
+            <h6>Workshop Kurikulum Merdeka Bagi Wakil Kurikulum:</h6><br>
+            <ul>
+                <li>• Workshop ini ditujukan kepada Wakil Kurikulum SMA/SMK Negeri dan Swasta di Wilayah 2 Jakarta Selatan.</li>
+                <li>• Fokus pada pembahasan tentang Kurikulum Merdeka, yang merupakan inisiatif pendidikan terbaru di Indonesia.</li>
+                <li>• Peserta akan mendapatkan pemahaman mendalam tentang Kurikulum Merdeka beserta strategi implementasinya.</li>
+            </ul><br>
+            
+
+            <h3>Reach the organizer</h3><br>
+            <div class="group-info d-flex mt-3">
+                <i class="ti-mobile mr-3 color-primary icon"></i><span class="description-event">087334263843</span>
+            </div>
+            <div class="group-info d-flex mt-3">
+                <i class="ti-email mr-3 color-primary icon"></i><span class="description-event">johndoe@gmail.com</span>
+            </div>
+            <a href="#" class="btn btn-book-now">Book Now</a>
+        </div>
+    </div>
+</div>
+
+<div class="map-container">
+    <div class="map-wrapper">
+        <div class="location-text"><h3>Location</h3></div>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3161.6262746699147!2d106.78878891571958!3d-6.226394285474659!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f82f9ce6f0a5%3A0x6822a3d87a02d2d1!2sUniversity%20of%20Indonesia!5e0!3m2!1sen!2sid!4v1610977185972!5m2!1sen!2sid" width="100%" height="400" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+    </div>
+</div>
+
 @endsection
