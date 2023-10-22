2ff09b57a7181b4dc62a0ea7ec3e792fb7cf4f10
alvincf10 <alvinwijaya446@gmail.com>
2023-10-22 18:16:18 +0700

    update not found event

--- resources/views/layouts/front/event/all-event.blade.php
+++ resources/views/layouts/front/event/all-event.blade.php
@@ -4,74 +4,83 @@
 
 <div class="container mt-5">
     <div class="row all-event-container">
-        <div class="event-filter col-md-4 col-12 me-4">
+        <div class="event-filter filter-collapsed col-md-4 col-12 me-4">
             <div class="d-flex flex-column">
-              <h4><b>Filters</b> </h4>
-              <button id="toggle-filter" class="btn btn-primary mt-3 w-100 d-md-none">Toggle Filter</button>
-              <form action="{{route('eventFront.filter')}}" method="POST">
-                @csrf
-                <div class="col-3 text-nowrap mt-3">
-                  <h4>Date</h4>
-                    <div class="form-check mt-3 mb-3">
-                      <input type="radio" class="form-check-input" id="date-month" name="date-option" value="this-month">
-                      <label class="form-check-label" for="date-month">This Month</label>
-                    </div>
-                    <div class="form-check mt-3 mb-3">
-                      <input type="radio" class="form-check-input" id="date-nextmonth" name="date-option" value="next-month">
-                      <label class="form-check-label" for="date-nextmonth">Next Month</label>
-                    </div>
+                <div class="toggle-minimizer d-flex justify-content-between">
+                    <h4><b>Filters</b></h4>
+                    <button id="toggle-filter" class="btn btn-primary btn-sm d-md-none">Toggle Filter</button>
                 </div>
-                <div class="col-3 mt-4">
-                  <h4>Price</h4>
-                  <div class="form-check mt-3 mb-3">
-                    <input type="radio" class="form-check-input" id="ticket-free" name="price-option" value="free">
-                    <label class="form-check-label" for="ticket-free">Free</label>
-                  </div>
-                  <div class="form-check mt-3 mb-3">
-                    <input type="radio" class="form-check-input" id="ticket-paid" name="price-option" value="paid">
-                    <label class="form-check-label" for="ticket-paid">Paid</label>
-                  </div>
+              <form action="{{ route('eventFront.filter') }}" method="POST">
+                    @csrf
+                    <div class="col-3 text-nowrap mt-3">
+                    <h4>Date</h4>
+                    <div class="form-check mt-3 mb-3">
+                        <input type="radio" class="form-check-input" id="date-month" name="date-option" value="this-month">
+                        <label class="form-check-label" for="date-month">This Month</label>
+                    </div>
+                    <div class="form-check mt-3 mb-3">
+                        <input type="radio" class="form-check-input" id="date-nextmonth" name="date-option" value="next-month">
+                        <label class="form-check-label" for="date-nextmonth">Next Month</label>
+                    </div>
+                    </div>
+                    <div class="col-3 mt-4">
+                    <h4>Price</h4>
+                    <div class="form-check mt-3 mb-3">
+                        <input type="radio" class="form-check-input" id="ticket-free" name="price-option" value="free">
+                        <label class="form-check-label" for="ticket-free">Free</label>
+                    </div>
+                    <div class="form-check mt-3 mb-3">
+                        <input type="radio" class="form-check-input" id="ticket-paid" name="price-option" value="paid">
+                        <label class="form-check-label" for="ticket-paid">Paid</label>
+                    </div>
+                    </div>
+                    <div class="col-3 mt-4">
+                    <h4>Category</h4>
+                    <div class="form-check mt-3 mb-3">
+                        <input type="radio" class="form-check-input" id="category-conference" name="category-option" value="1">
+                        <label class="form-check-label" for="category-conference">Conference</label>
+                    </div>
+                    <div class="form-check mt-3 mb-3">
+                        <input type="radio" class="form-check-input" id="category-workshop" name="category-option" value="2">
+                        <label class="form-check-label" for="category-workshop">Workshop</label>
+                    </div>
+                    <div class="form-check mt-3 mb-3">
+                        <input type="radio" class="form-check-input" id="category-sports" name="category-option" value="3">
+                        <label class="form-check-label" for="category-sports">Sports</label>
+                      </div>
+                      <div class="form-check mt-3 mb-3">
+                        <input type="radio" class="form-check-input" id="category-social" name="category-option" value="4">
+                        <label class="form-check-label" for="category-social">Social</label>
+                      </div>
+                      <div class="form-check mt-3 mb-3">
+                        <input type="radio" class="form-check-input" id="category-art" name="category-option" value="5">
+                        <label class="form-check-label" for="category-art">Art</label>
+                      </div>
+                      <div class="form-check mt-3 mb-3">
+                        <input type="radio" class="form-check-input" id="category-fashion" name="category-option" value="6">
+                        <label class="form-check-label" for="category-fashion">Fashion</label>
+                      </div>
+                      <div class="form-check mt-3 mb-3">
+                        <input type="radio" class="form-check-input" id="category-fashion" name="category-option" value="">
+                        <label class="form-check-label" for="category-fashion">Game</label>
+                      </div>
+                    </div>
+            </div>
+                <div class="group-apply w-100">
+                    <button type="submit" class="btn btn-primary mt-3 w-100">Apply Filters</button>
                 </div>
-                <div class="col-3 mt-4">
-                  <h4>Category</h4>
-                    <div class="form-check mt-3 mb-3">
-                      <input type="radio" class="form-check-input" id="category-conference" name="category-option" value="1">
-                      <label class="form-check-label" for="category-conference">Conference</label>
-                    </div>
-                    <div class="form-check mt-3 mb-3">
-                      <input type="radio" class="form-check-input" id="category-workshop" name="category-option" value="2">
-                      <label class="form-check-label" for="category-workshop">Workshop</label>
-                    </div>
-                    <div class="form-check mt-3 mb-3">
-                      <input type="radio" class="form-check-input" id="category-sports" name="category-option" value="3">
-                      <label class="form-check-label" for="category-sports">Sports</label>
-                    </div>
-                    <div class="form-check mt-3 mb-3">
-                      <input type="radio" class="form-check-input" id="category-social" name="category-option" value="4">
-                      <label class="form-check-label" for="category-social">Social</label>
-                    </div>
-                    <div class="form-check mt-3 mb-3">
-                      <input type="radio" class="form-check-input" id="category-art" name="category-option" value="5">
-                      <label class="form-check-label" for="category-art">Art</label>
-                    </div>
-                    <div class="form-check mt-3 mb-3">
-                      <input type="radio" class="form-check-input" id="category-fashion" name="category-option" value="6">
-                      <label class="form-check-label" for="category-fashion">Fashion</label>
-                    </div>
-                    <div class="form-check mt-3 mb-3">
-                      <input type="radio" class="form-check-input" id="category-fashion" name="category-option" value="">
-                      <label class="form-check-label" for="category-fashion">Game</label>
-                    </div>
-                </div>
-              </div>
-              <div class="group-apply w-100">
-                  <button type="submit" class="btn btn-primary mt-3 w-100">Apply Filters</button>
-              </div>
               </form>
         </div>
-
         <div class="list-event col-md-8 col-12">
             @if (isset($filteredProgram))
+                @if (count($filteredProgram) == 0)
+                    <div class="d-flex justify-content-center align-items-center flex-column">
+                        <div class="img-not-found">
+                        <img src="{{asset('image/not-found.png')}}" width="300">
+                        </div>
+                        <h3 class="title-not-found mt-3">Sorry, no matching results found</h3>
+                    </div>
+                @endif
                 @foreach ($filteredProgram as $programs )
                 <a href="{{route('event.detail',['id'=>$programs->id])}}">
                     <div class="content-container mb-4">
@@ -151,12 +160,14 @@
 
 <script>
     document.addEventListener("DOMContentLoaded", function() {
-        const filter = document.querySelector(".event-filter");
-        const toggleButton = document.getElementById("toggle-filter");
+    const filter = document.querySelector(".event-filter");
+    const toggleButton = document.getElementById("toggle-filter");
 
+    if (toggleButton) {
         toggleButton.addEventListener("click", function() {
-            filter.classList.toggle("filter-collapsed");
+        filter.classList.toggle("filter-collapsed");
         });
+    }
     });
 </script>
 
